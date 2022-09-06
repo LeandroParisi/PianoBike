af55676166442c47af0159dea496eb3a1c847b5c
LeandroParisi <lepcbelisario@gmail.com>
2022-09-06 06:47:57 -0300

    updates footer

--- src/components/MainComponents/Footer/Footer.tsx
+++ src/components/MainComponents/Footer/Footer.tsx
@@ -6,7 +6,7 @@ import * as styles from './Footer.module.scss'
 const Footer = () => (
     <footer className={classNames(globals.fullWidth, styles.footer)}>
       <div className={styles.section, styles.left}>
-        <p>© 2022 PianoBike - non profit organization</p>
+        <p>© 2022 PianoBike Org.</p>
       </div>
       <div className={styles.section, styles.center}>
         <p>CNPJ: 29.418.255-0001-50</p>
