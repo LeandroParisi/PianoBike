74fdd0ed3be23b93ae8615a9aa57b1bdcfb7c374
LeandroParisi <lepcbelisario@gmail.com>
2022-09-06 07:01:49 -0300

    updates footer

--- src/components/MainComponents/Footer/Footer.tsx
+++ src/components/MainComponents/Footer/Footer.tsx
@@ -5,13 +5,13 @@ import * as styles from './Footer.module.scss'
 
 const Footer = () => (
     <footer className={classNames(globals.fullWidth, styles.footer)}>
-      <div className={styles.section, styles.left}>
+      <div className={classNames(styles.section, styles.left)}>
         <p>© 2022 PianoBike Org.</p>
       </div>
-      <div className={styles.section, styles.center}>
+      <div className={classNames(styles.section, styles.center)}>
         <p>CNPJ: 29.418.255-0001-50</p>
       </div>
-      <div className={styles.section, styles.right}>
+      <div className={classNames(styles.section, styles.right)}>
         <p>Produção cultural: <a href='mailto:gestao@ohgloriaproducoes.com.br'>gestao@ohgloriaproducoes.com.br</a></p>
         <p><a target="_blank" href='https://www.instagram.com/piano.bike/'>More info</a></p>
       </div>
